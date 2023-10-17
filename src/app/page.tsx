3ccbc0126dbcdb624b0d819a802a3a040cc3a310
RT <rustytrim11@gmail.com>
2023-10-17 15:17:23 -0400

    Restarting with shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Navbar from "@/components/Navbar";
-
 export default function Home() {
   return (
-    <div className="body home-grad">
-      <Navbar/>
+    <div className="body">
+      
     </div>
   )
 }
