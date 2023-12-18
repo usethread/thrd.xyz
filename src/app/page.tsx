a2efbdf4f8e81d298a749fed1f4433b8765ef6a2
BrydenIsNotSmart <brydenwcgames@gmail.com>
2023-12-18 14:34:32 -0600

    Redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,44 @@
+"use client"
+
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
-import { Badge } from "@/components/ui/badge";
-import Countdown from "@/components/Countdown";
-import { Input } from "@/components/ui/input";
+import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleOpenWebApp = () => {
+    router.push('https://web.strafe.chat');
+  };
+
+  const handleJoinDiscord = () => {
+    router.push('https://discord.com/invite/XPyjQXaXKG');
+  };
+
   return (
     <div className="min-h-screen">
       <Navbar />
-      <div className="flex flex-col gap-4 pt-[85px]">
-        <div className="max-w-7xl mx-auto py-32 px-3 xl:px-0 xl:min-h-screen xl:py-0">
-          <div className="flex gap-2 items-center space-x-44">
-            <div className="flex flex-col gap-6 w-full xl:w-1/2">
+      <div className="flex flex-col gap-4 pt-[75px] text-center w-full">
+        <div className="max-w-7xl mx-auto py-32 px-3 xl:px-0 xl:min-h-screen xl:py-0 text-center">
+          <div className="flex gap-2 flex-row items-center space-x-44 text-center">
+            <div className="flex flex-col gap-6 text-center w-full">
               <div className="">
-                <Countdown />
-                <h1 className="xl:text-6xl md:text-3xl text-left font-bold">
-                  No Censorship, Just Communication.
+                <h1 className="xl:text-7xl font-bold text-center">
+                  No Censorship,<br></br> Just Communication.
                 </h1>
-                <p className="mt-2 text-[17px]">
+                <p className="mt-2 text-[17px] text-center">
                   StrafeChat is a web application focused on no censorship. We
-                  believe that people with any belief, opinion, or view should
+                  believe that people with any belief,<br></br> opinion, or view should
                   be able to communicate with each other freely.
                 </p>
-                <div className="flex flex-row mt-3 items-center gap-2">
-                  <Input
-                   disabled
-                    placeholder="john.doe123@example.com"
-                    type="email"
-                  />
-                  <Button disabled className="w-[19rem] disabled" variant={'default'}>Subscribe To Newsletter</Button>
+                <div className="flex flex-col mt-5 items-center gap-2 w-full text-center whitespace-nowrap">
+                <span>
+                <Button onClick={handleOpenWebApp} className="bg-[#232428] text-white font-bold rounded-[5px]" size="lg">Open Web App</Button> &nbsp;&nbsp;
+                <Button onClick={handleJoinDiscord} className="bg-[white] text-gray-600 font-bold rounded-[5px]" size="lg">Join our Discord</Button>
+                </span>
                 </div>
               </div>
             </div>
-            <Image
-            className="h-[200px] xl:block hidden rounded-xl"
-              src={"/strafebanner.jpg"}
-              width={500}
-              height={500}
-              alt="StrafeChat"
-            />
           </div>
         </div>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,21 +15,24 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import { GiHamburgerMenu } from "./Icons";
 import { DropdownMenuContent, DropdownMenuLabel } from "./ui/dropdown-menu";
-import { Book, Info, LogIn } from "lucide-react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBlog, faCircleInfo, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
   return (
     <nav className="navbar max-w-7xl flex items-center mx-auto px-4 gap-4 md:px-0">
       <div className="flex items-center gap-2.5">
-      <img style={{ borderRadius: "25%"}} className="w-10 h-10 rounded-md" src="/favicon.ico" alt="Logo" />
-      <h1 className="text-2xl font-bold">Strafe.chat</h1>
+          <img style={{ borderRadius: "25%"}} className="w-10 h-10 rounded-md" src="/favicon.ico" alt="Logo" />
+          <Link href="/">
+          <h1 className="text-2xl font-bold">Strafe.chat</h1>
+          </Link>
       </div>
       <NavigationMenu className="hidden md:flex">
         <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              <Book size={"1.25em"} />
+          <NavigationMenuItem className="hover:rounded-[5px]">
+            <Link href="https://blog.strafe.chat" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()} >
+              <FontAwesomeIcon icon={faBlog} size="1x" />
               &nbsp;Blog
               </NavigationMenuLink>
             </Link>
@@ -37,7 +40,7 @@ export default function Navbar() {
           <NavigationMenuItem>
             <Link href="/about" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              <Info size={"1.25em"} />
+              <FontAwesomeIcon icon={faCircleInfo} size="1x" />
               &nbsp;About
               </NavigationMenuLink>
             </Link>
@@ -45,7 +48,7 @@ export default function Navbar() {
           <NavigationMenuItem>
             <Link href="https://web.strafe.chat/login" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              <LogIn size={"1.25em"} /> 
+              <FontAwesomeIcon icon={faRightToBracket} size="1x" />
                 &nbsp;Login
               </NavigationMenuLink>
             </Link>
@@ -62,19 +65,19 @@ export default function Navbar() {
           <DropdownMenuContent className="w-fit mr-2 p-2 flex flex-col gap-1">
             <DropdownMenuItem>
               <Link href={"/blog"} className="flex text-md gap-1 items-center">
-                <Book size={"1.25em"} />
+              <FontAwesomeIcon icon={faBlog} size="1x" />
                 <span>Blog</span>
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem>
               <Link href={"/about"} className="flex text-md gap-1 items-center">
-                <Info size={"1.25em"} />
+              <FontAwesomeIcon icon={faCircleInfo} size="1x" />
                 <span>About</span>
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem>
               <Link href={"/login"} className="flex text-md gap-1 items-center">
-                <LogIn size={"1.25em"} />
+              <FontAwesomeIcon icon={faRightToBracket} size="1x" />
                 <span>Login</span>
               </Link>
             </DropdownMenuItem>
